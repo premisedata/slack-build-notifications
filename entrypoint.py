bc5c9484efc7f8e031f398883df06a3abbbff1dc
anovis <anovis@ymail.com>
2021-12-01 10:22:51 -0500

    switch to outcome

--- entrypoint.py
+++ entrypoint.py
@@ -6,19 +6,25 @@ from slack_sdk.errors import SlackApiError
 url = sys.argv[4] 
 webhook = WebhookClient(url)
 
-def message_builder(status, project, build, repo):
+def message_builder(outcome, project, build, repo):
+    color = "#000000"
     success_color = "#4BB543"
     failure_color = "#FF0000"
-    # need to account for different statuses? cloud run vs maven
+    
+    if outcome == "success":
+        color = success_color
+    if outcome == "failure":
+        color = failure_color
+    # need to account for different outcomees? cloud run vs maven
     org = repo.split("/")[0]
     service = repo.split("/")[1]
     message = [
         {
-            "color": success_color,
+            "color": color,
             "blocks": [
                 {
                     "type": "section",
-                    "text": {"type": "mrkdwn", "text": f"Status: {status}"},
+                    "text": {"type": "mrkdwn", "text": f"outcome: {outcome}"},
                 },
                 {
                     "type": "section",
@@ -49,13 +55,13 @@ def message_builder(status, project, build, repo):
 
 
 if __name__ == "__main__":
-    status = sys.argv[1]
+    outcome = sys.argv[1]
     project = sys.argv[2]
     build = sys.argv[3]
     repo = sys.argv[5]
 
     try:
-        formatted_message = message_builder(status, project, build, repo)
+        formatted_message = message_builder(outcome, project, build, repo)
         response = webhook.send(
             attachments=formatted_message
         )
